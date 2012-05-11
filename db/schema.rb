dc72e215240a44dfb2fe930952812d8c7ccb7aaf
Balazs Kiss <balazsnet@gmail.com>
2012-05-11 08:59:27 +0200

    hibák elhárítva

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,9 @@ ActiveRecord::Schema.define(:version => 20120509134810) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
