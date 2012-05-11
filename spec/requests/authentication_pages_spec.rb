a95474595d01b5a8add7df65949f68653a050359
Balazs Kiss <balazsnet@gmail.com>
2012-05-11 11:17:45 +0200

    végleg befejezve

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,6 +28,8 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
       before do
         fill_in "Email",    with: user.email
         fill_in "Password", with: user.password
@@ -36,13 +38,40 @@ describe "Authentication" do
 
     describe "followed by signout" do
         before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+        #it { should have_link('Sign in') }
       end
+    end
+  end
 
-      it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          #it { should have_selector('title', text: 'Sign in') }
+        end
+
+      describe "as non-admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:non_admin) { FactoryGirl.create(:user) }
+        before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }        
+        end
+      end
+      end
     end
   end
 end
